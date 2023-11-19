494b475ad7e4ff1eb908581e805604df925a2a26
Dev Asheesh Chopra <chopradevasheesh@gmail.com>
2023-11-19 20:28:12 +0530

    Pushing the app

---
+++ app/modelutil.py
@@ -0,0 +1,47 @@
+import os 
+import tensorflow as tf
+from keras import Sequential 
+from keras.layers import Conv3D, LSTM, Dense, Dropout, Bidirectional, MaxPool3D, Activation, Reshape, SpatialDropout3D, BatchNormalization, TimeDistributed, Flatten
+from keras.optimizers.legacy import Adam
+
+def CTCLoss(y_true, y_pred):
+    batch_len = tf.cast(tf.shape(y_true)[0], dtype="int64")
+    input_length = tf.cast(tf.shape(y_pred)[1], dtype="int64")
+    label_length = tf.cast(tf.shape(y_true)[1], dtype="int64")
+
+    input_length = input_length * tf.ones(shape=(batch_len, 1), dtype="int64")
+    label_length = label_length * tf.ones(shape=(batch_len, 1), dtype="int64")
+
+    loss = tf.keras.backend.ctc_batch_cost(y_true, y_pred, input_length, label_length)
+    return loss
+
+def load_model() -> Sequential: 
+    model = Sequential()
+
+    model.add(Conv3D(128, 3, input_shape=(75,46,140,1), padding='same'))
+    model.add(Activation('relu'))
+    model.add(MaxPool3D((1,2,2)))
+
+    model.add(Conv3D(256, 3, padding='same'))
+    model.add(Activation('relu'))
+    model.add(MaxPool3D((1,2,2)))
+
+    model.add(Conv3D(75, 3, padding='same'))
+    model.add(Activation('relu'))
+    model.add(MaxPool3D((1,2,2)))
+
+    model.add(TimeDistributed(Flatten()))
+
+    model.add(Bidirectional(LSTM(128, kernel_initializer='Orthogonal', return_sequences=True)))
+    model.add(Dropout(.5))
+
+    model.add(Bidirectional(LSTM(128, kernel_initializer='Orthogonal', return_sequences=True)))
+    model.add(Dropout(.5))
+
+    model.add(Dense(41, kernel_initializer='he_normal', activation='softmax'))
+
+    model.compile(optimizer=Adam(learning_rate=0.0001), loss=CTCLoss)
+    
+    model.load_weights(os.path.join('app', 'models', 'checkpoint'))
+
+    return model
---
+++ app/stapp.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import os 
+import tensorflow as tf 
+from utils import load_data, num_to_char
+from modelutil import load_model
+
+st.set_page_config(layout='wide')
+
+with st.sidebar: 
+    st.image('https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png')
+    st.title('LipSense')
+    st.info('Experience the power of cutting-edge technology as we bring you a unique solution for bridging communication gaps. Our deep learning model can interpret lip movements in real-time, unlocking a new dimension of inclusivity for individuals with hearing impairments.')
+
+st.title('LipSense Project') 
+data_dir = os.path.join('app', 'data', 's1')
+options = os.listdir(data_dir)
+selected_video = st.selectbox('Choose video', options)
+
+col1, col2 = st.columns(2)
+
+if options: 
+
+    with col1: 
+        st.info('The video below displays the converted video in mp4 format')
+        file_path = os.path.join('app', 'data', 's1', selected_video)
+        os.system(f'ffmpeg -i {file_path} -vcodec libx264 app/test_video.mp4 -y')
+
+        fp = os.path.join('app', 'data', 's1')
+
+        video = open('app/test_video.mp4', 'rb') 
+        video_bytes = video.read() 
+        st.video(video_bytes)
+
+
+    with col2: 
+        video, annotations = load_data(tf.convert_to_tensor(file_path))
+        print(fp)
+        st.info('This is the output of the machine learning model as tokens')
+        model = load_model()
+        yhat = model.predict(tf.expand_dims(video, axis=0))
+        decoder = tf.keras.backend.ctc_decode(yhat, [75], greedy=True)[0][0].numpy()
+        st.text(decoder)
+
+        st.info('Decode the raw tokens into words')
+        converted_prediction = tf.strings.reduce_join(num_to_char(decoder)).numpy().decode('utf-8')
+        st.text(converted_prediction)
+        
+        
+        yhat = None
+        decoder = None
+        
+        
